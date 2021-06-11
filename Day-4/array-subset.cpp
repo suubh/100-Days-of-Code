e81ee1f84f9e3dfa82b1da8b6ec26d44df1a4a66
suubh <subhdec99@gmail.com>
2021-06-12 03:13:28 +0530

    Added details

--- Day-4/array-subset.cpp
+++ Day-4/array-subset.cpp
@@ -1,7 +1,33 @@
 #include<iostream>
+#include<math.h>
 using namespace std;
 int main(){
+	//SUBSET ARRAY SUM BY GENERATING ALL THE SUBSETS
 	
+	int n,s;
+	cout << "Enter the value of n - "<<endl;
+	cin>>n;
+	int arr[n];
+	cout << "Enter the value in Array - "<<endl;
+	for(int i=0;i<n;i++){
+		cin >> arr[i];
+	}
+	cout << "Enter the Sum required = "<<endl;
+	cin >> s;
+	
+	//GENERATE ALL THE SUBSEQUENCE ,CHECK THE CONDITION .
+	for(int i=1;i<pow(2,n);i++){	
+		int sum=0;
+		for(int j=0;j<n;j++){
+			if((i>>j)&1){
+				sum+=arr[j];
+			}
+				if(sum ==s){
+					cout << "Yes"<<endl;
+				}
+		}
+	}
+
 	
 	
 	return 0;
--- Day-4/strings.cpp
+++ Day-4/strings.cpp
@@ -1,14 +1,25 @@
 #include<iostream>
-#include<string.h>
+#include<string>
 using namespace std;
 int main(){
+	
 	string s;
 	cin >> s;
+	
 	//one way
 	cout <<s;
+	
 	//other way
 	for(int i=0;i<s.length();i++){
 		cout << s[i];
 	}
+	//USING GETLINE(),PUSH_BACK(),POP_BACK()
+	
+	
+	s.push_back('h');
+	cout<< "Added char at the end of the String - "<<s<<endl;
+	
+//	str.pop_back(); ADDED IN C++11
+//	cout << "Deleting the last char from the String - "<<str<<endl; 
 	
 }
